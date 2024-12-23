3b554c2e8cf6ba86ee2fe76077c796f27d1ee2d1
alrazak <abdrzak1129@gmail.com>
2024-12-24 00:46:22 +0700

    add: appBar detail Card

--- lib/item_widget.dart
+++ lib/item_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_app/data.dart';
+import 'package:grocery_app/product_detail.dart';
 
 class ItemWidget extends StatelessWidget {
   const ItemWidget({
@@ -10,111 +11,120 @@ class ItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      shadowColor: Colors.green,
-      color: Colors.white,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Hero(
-            tag: product.image,
-            child: SizedBox(
-              width: 150,
-              child: Image.asset(product.image),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context){
+          return ProductDetail(
+            product: product,
+          );
+        }));
+      },
+      child: Card(
+        elevation: 2,
+        shadowColor: Colors.green,
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Hero(
+              tag: product.image,
+              child: SizedBox(
+                width: 150,
+                child: Image.asset(product.image),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          Text(
-            'Rp ${product.price}',
-            style: const TextStyle(
-              color: Colors.green,
-              fontSize: 20,
-              fontWeight: FontWeight.w800,
+            const SizedBox(
+              height: 6,
             ),
-          ),
-          const SizedBox(
-            height: 2,
-          ),
-          Text(
-            product.name,
-            style: const TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w800,
+            Text(
+              'Rp ${product.price}',
+              style: const TextStyle(
+                color: Colors.green,
+                fontSize: 20,
+                fontWeight: FontWeight.w800,
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 2,
-          ),
-          Text(
-            product.quantity,
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w300,
+            const SizedBox(
+              height: 2,
             ),
-          ),
-          const SizedBox(
-            height: 4,
-          ),
-          const Divider(
-            height: 1,
-            color: Colors.grey,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                children: const [
-                  Icon(
-                    Icons.add_shopping_cart,
-                    size: 20,
-                    color: Colors.green,
-                  ),
-                  SizedBox(
-                    width: 2,
-                  ),
-                  Text(
-                    'Beli',
-                    style: TextStyle(
+            Text(
+              product.name,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            const SizedBox(
+              height: 2,
+            ),
+            Text(
+              product.quantity,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            const Divider(
+              height: 1,
+              color: Colors.grey,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.add_shopping_cart,
+                      size: 20,
                       color: Colors.green,
-                      fontSize: 16,
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                width: 8,
-              ),
-              Row(
-                children: const [
-                  Icon(
-                    Icons.remove_circle_outline,
-                    size: 18,
-                    color: Colors.green,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 5),
-                    child: Text('0'),
-                  ),
-                  Icon(
-                    Icons.add_circle_outline,
-                    size: 18,
-                    color: Colors.green,
-                  )
-                ],
-              )
-            ],
-          )
-        ],
-      ),
+                    SizedBox(
+                      width: 2,
+                    ),
+                    Text(
+                      'Beli',
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.remove_circle_outline,
+                      size: 18,
+                      color: Colors.green,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 5),
+                      child: Text('0'),
+                    ),
+                    Icon(
+                      Icons.add_circle_outline,
+                      size: 18,
+                      color: Colors.green,
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
---
+++ lib/product_detail.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/data.dart';
+
+class ProductDetail extends StatelessWidget {
+  const ProductDetail({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 1,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text(
+          'Detail',
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 8.0),
+            child: Icon(
+              Icons.favorite_border,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
