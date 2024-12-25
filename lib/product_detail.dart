c2b76a0054f0c5ea264f5c2b37eacd1fb27b9a7d
alrazak <abdrzak1129@gmail.com>
2024-12-25 19:59:49 +0700

    add: body detail Card

--- lib/product_detail.dart
+++ lib/product_detail.dart
@@ -38,6 +38,135 @@ class ProductDetail extends StatelessWidget {
           ),
         ],
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            product.name,
+            style: const TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Center(
+            child: Hero(
+              tag: product.image,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.45,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(product.image), fit: BoxFit.contain
+                  )
+                ),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.grey[200],
+            padding: const EdgeInsets.only(left: 20, top: 20),
+            child: Row(
+              children: [
+                Text(
+                  'Rp ${product.price}',
+                  style: const TextStyle(
+                    color: Colors.green,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '/${product.quantity}',
+                  style: const TextStyle(
+                    color: Colors.green,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.grey[200],
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                product.description,
+                style: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.justify,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 5,
+              ),
+            ),
+          ),
+          Container(
+            height: 100,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            alignment: Alignment.bottomCenter,
+            color: Colors.grey,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.remove_circle_outline,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ),
+                      const Text(
+                        '1',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.add_circle_outline,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    width: 190,
+                    height: 60,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(25),
+                      ),
+                      color: Colors.green,
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Pesan',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
