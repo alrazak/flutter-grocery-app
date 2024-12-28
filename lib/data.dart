f42d4c7fc36af2e3b8dc85acd1173985e42ea572
alrazak <abdrzak1129@gmail.com>
2024-12-28 22:09:55 +0700

    add: ProductEvent

--- lib/data.dart
+++ lib/data.dart
@@ -1,74 +1,74 @@
-class Product {
-  final String name;
-  final String price;
-  final String quantity;
-  final String image;
-  final String description;
-
-  Product(
-      this.name,
-      this.price,
-      this.quantity,
-      this.image,
-      this.description,
-      );
-}
-
-final List<Product> allData = [
-  Product(
-      'Apel',
-      '55.000',
-      '1kg',
-      'assets/apel.jpg',
-      'Apel adalah buah yang kaya akan vitamin, terutama vitamin C. Buah ini memiliki rasa manis dengan sedikit asam, menjadikannya pilihan yang sempurna untuk camilan sehat dan segar. Cocok dimakan langsung atau diolah menjadi berbagai hidangan.'
-  ),
-  Product(
-      'Anggur',
-      '85.000',
-      '1kg',
-      'assets/anggur.jpg',
-      'Anggur memiliki rasa manis yang khas dan sering digunakan sebagai camilan sehat atau bahan dasar dalam pembuatan jus dan wine. Kaya akan antioksidan dan vitamin C.'
-  ),
-  Product(
-      'Jeruk',
-      '45.000',
-      '1kg',
-      'assets/jeruk.jpg',
-      'Jeruk adalah sumber vitamin C yang sangat baik. Dengan rasa manis dan segar, jeruk sering dijadikan sebagai jus atau dimakan langsung.'
-  ),
-  Product(
-      'Nanas',
-      '30.000',
-      '1 buah',
-      'assets/nanas.jpg',
-      'Nanas memiliki rasa asam manis yang menyegarkan. Buah ini sering digunakan dalam hidangan tropis, salad, atau jus segar.'
-  ),
-  Product(
-      'Wortel',
-      '25.000',
-      '1kg',
-      'assets/wortel.jpg',
-      'Wortel adalah sayuran yang kaya akan beta-karoten, yang baik untuk kesehatan mata. Biasanya dimasak dalam sup, tumisan, atau dijadikan jus.'
-  ),
-  Product(
-      'Tomat',
-      '20.000',
-      '1kg',
-      'assets/tomat.jpg',
-      'Tomat adalah sumber vitamin C dan antioksidan. Cocok digunakan dalam masakan, salad, atau dijadikan jus.'
-  ),
-  Product(
-      'Jagung',
-      '15.000',
-      '3 tongkol',
-      'assets/jagung.jpg',
-      'Jagung adalah sumber karbohidrat yang sering dimasak sebagai jagung bakar, direbus, atau dijadikan bahan masakan lainnya.'
-  ),
-  Product(
-      'Cabai',
-      '60.000',
-      '1kg',
-      'assets/cabai.jpg',
-      'Cabai adalah bahan utama dalam berbagai masakan pedas. Mengandung capsaicin yang bermanfaat untuk meningkatkan metabolisme tubuh.'
-  ),
-];
+// class Product {
+//   final String name;
+//   final String price;
+//   final String quantity;
+//   final String image;
+//   final String description;
+//
+//   Product(
+//       this.name,
+//       this.price,
+//       this.quantity,
+//       this.image,
+//       this.description,
+//       );
+// }
+//
+// final List<Product> allData = [
+//   Product(
+//       'Apel',
+//       '55.000',
+//       '1kg',
+//       'assets/apel.jpg',
+//       'Apel adalah buah yang kaya akan vitamin, terutama vitamin C. Buah ini memiliki rasa manis dengan sedikit asam, menjadikannya pilihan yang sempurna untuk camilan sehat dan segar. Cocok dimakan langsung atau diolah menjadi berbagai hidangan.'
+//   ),
+//   Product(
+//       'Anggur',
+//       '85.000',
+//       '1kg',
+//       'assets/anggur.jpg',
+//       'Anggur memiliki rasa manis yang khas dan sering digunakan sebagai camilan sehat atau bahan dasar dalam pembuatan jus dan wine. Kaya akan antioksidan dan vitamin C.'
+//   ),
+//   Product(
+//       'Jeruk',
+//       '45.000',
+//       '1kg',
+//       'assets/jeruk.jpg',
+//       'Jeruk adalah sumber vitamin C yang sangat baik. Dengan rasa manis dan segar, jeruk sering dijadikan sebagai jus atau dimakan langsung.'
+//   ),
+//   Product(
+//       'Nanas',
+//       '30.000',
+//       '1 buah',
+//       'assets/nanas.jpg',
+//       'Nanas memiliki rasa asam manis yang menyegarkan. Buah ini sering digunakan dalam hidangan tropis, salad, atau jus segar.'
+//   ),
+//   Product(
+//       'Wortel',
+//       '25.000',
+//       '1kg',
+//       'assets/wortel.jpg',
+//       'Wortel adalah sayuran yang kaya akan beta-karoten, yang baik untuk kesehatan mata. Biasanya dimasak dalam sup, tumisan, atau dijadikan jus.'
+//   ),
+//   Product(
+//       'Tomat',
+//       '20.000',
+//       '1kg',
+//       'assets/tomat.jpg',
+//       'Tomat adalah sumber vitamin C dan antioksidan. Cocok digunakan dalam masakan, salad, atau dijadikan jus.'
+//   ),
+//   Product(
+//       'Jagung',
+//       '15.000',
+//       '3 tongkol',
+//       'assets/jagung.jpg',
+//       'Jagung adalah sumber karbohidrat yang sering dimasak sebagai jagung bakar, direbus, atau dijadikan bahan masakan lainnya.'
+//   ),
+//   Product(
+//       'Cabai',
+//       '60.000',
+//       '1kg',
+//       'assets/cabai.jpg',
+//       'Cabai adalah bahan utama dalam berbagai masakan pedas. Mengandung capsaicin yang bermanfaat untuk meningkatkan metabolisme tubuh.'
+//   ),
+// ];
